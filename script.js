35f4a258a05e0b2d0f543020447889c2b6c6c196
Sanyam Jain <Sanyam0029@gmail.com>
2023-04-25 19:16:20 +0530

    Temprature Converter

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById("tempratureConverter").onclick = function(){
     let temp = document.getElementById("inputTemprature").value;
     if(document.getElementById("toFahrenite").checked){
-        document.getElementById("message").innerHTML = `Temprature in Fahrenite is :: ${temp*9/5 + 32}`;
+        document.getElementById("message").innerHTML = `Temprature in Fahrenite is :: ${temp*9/5 + 32} °F`;
     }
     else if(document.getElementById("toCelsius")){
-        document.getElementById("message").innerHTML = `Temprature in Celsius is :: ${(temp-32)*5/9}`;
+        document.getElementById("message").innerHTML = `Temprature in Celsius is :: ${(temp-32)*5/9} °C`;
     }
     else{{
         document.getElementById("message").innerHTML = "Choose a valid Option";
